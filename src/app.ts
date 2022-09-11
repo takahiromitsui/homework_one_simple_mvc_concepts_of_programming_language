fc69ec527e4107faf22c95a019711065798a8a6c
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-09-11 16:38:54 +0200

    Create Post method

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import userRoutes from './routes/user';
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(userRoutes);
 
 app.listen({ port: 3000 });
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -10,14 +10,16 @@ router.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../../src/', 'views', 'add-user.html'));
 });
 
-router.post('/', (req, res) => {
+router.post('/user', (req, res) => {
 	const username = req.body.username;
 	const newUser: User = {
 		id: new Date().toISOString(),
 		name: username,
-	};
+	}
 	users.push(newUser);
-	res.status(201).json({ message: 'User created', user: newUser });
-});
+	res.redirect('/user')
+})
+
+
 
 export default router;
