657d1764c453eeab9edcd7eda5ef5a9e890c26ea
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-09-11 17:20:26 +0200

    Add user page

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,20 +6,28 @@ const router = Router();
 const users: User[] = [];
 
 router.get('/', (req, res) => {
-	// res.sendFile(path.join(__dirname, '../../src/', 'views', 'add-user.html'));
-	res.render('add-user');
+	res.render('add-user', { pageTitle: 'Add User', path: '/' });
 });
 
 router.post('/user', (req, res) => {
 	const username = req.body.username;
+	const id = new Date().toISOString();
 	const newUser: User = {
-		id: new Date().toISOString(),
+		id: id,
 		name: username,
-	}
+	};
 	users.push(newUser);
-	res.redirect('/user')
-})
+	res.redirect(`/user/${id}`);
+});
 
-
+router.get('/user/:id', (req, res) => {
+	const id = req.params.id;
+	const user = users.find(user => user.id === id);
+	res.render('user', {
+		pageTitle: 'User',
+		path: '/user',
+		username: user?.name || '',
+	});
+});
 
 export default router;
