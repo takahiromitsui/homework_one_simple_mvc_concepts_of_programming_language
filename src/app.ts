b57141ee21097d915d18cef5ba24a796cc845f17
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-09-11 11:02:07 +0200

    Create User model

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import userRoutes from './routes/user';
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use('/user', userRoutes);
 
 app.listen({ port: 3000 });
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
+import { User } from '../../models/user';
 
 const router = Router();
 
+const users: User[] = [];
+
 router.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+router.post('/', (req, res) => {
+	const name = req.body.name;
+	const newUser: User = {
+		id: new Date().toISOString(),
+		name: name,
+	};
+	users.push(newUser);
 });
 
 export default router;
