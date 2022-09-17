287b5fd946d33ac4d2cd9b63b4ccd57ad2c82c69
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-09-17 15:45:54 +0200

    Make postUser dependency injectable

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,23 +1,41 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { customLogger, WinstonLevel } from '../logger';
 import { User } from '../models/user';
 
 const users: User[] = [];
 
+type ExpressRouteFunc = (
+	req: Request,
+	res: Response,
+	next?: NextFunction
+) => void | Promise<void>;
+
 export const getDefault = (req: Request, res: Response) => {
 	res.render('add-user', { pageTitle: 'Add User', path: '/' });
 };
 
-export const postUser = (req: Request, res: Response) => {
-	const username = req.body.username;
-	const id = new Date().toISOString();
-	const newUser: User = {
-		id: id,
-		name: username,
+export const postUser = (users: User[], userID: string): ExpressRouteFunc => {
+	return (req: Request, res: Response) => {
+		const username = req.body.username;
+		const newUser: User = {
+			id: userID,
+			name: username,
+		};
+		users.push(newUser);
+		res.redirect(`/user/${userID}`);
 	};
-	users.push(newUser);
-	res.redirect(`/user/${id}`);
 };
+
+// export const postUser = (req: Request, res: Response) => {
+// 	const username = req.body.username;
+// 	const id = new Date().toISOString();
+// 	const newUser: User = {
+// 		id: id,
+// 		name: username,
+// 	};
+// 	users.push(newUser);
+// 	res.redirect(`/user/${id}`);
+// };
 
 export const getUser = (req: Request, res: Response) => {
 	const id = req.params.id;
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { getDefault, getUser, postUser } from '../controllers/user';
+import { User } from '../models/user';
 
 const router = Router();
+const users: User[] = [];
 
 router.get('/', getDefault);
 
-router.post('/user', postUser);
+router.post('/user', postUser(users, new Date().toISOString()));
 
 router.get('/user/:id', getUser);
 
