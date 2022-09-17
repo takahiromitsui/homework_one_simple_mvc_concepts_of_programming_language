283f9972ee05c54647e98c0ab9da73dcf27dc2ca
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-09-17 11:39:34 +0200

    Improve log info

--- logger/index.ts
+++ logger/index.ts
@@ -1,15 +1,20 @@
 import { createLogger, format, transports } from 'winston';
 
-const { printf, combine, timestamp } = format;
+const { printf, combine, timestamp, colorize, errors } = format;
 
-const logFormat = printf(({ level, message, timestamp }) => {
-	return `${timestamp} ${level}: ${message}`;
+const logFormat = printf(({ level, message, timestamp, stack }) => {
+	return `${timestamp} ${level}: ${stack || message}`;
 });
 
 export const logger = createLogger({
-	format: combine(timestamp(), logFormat),
+	format: combine(
+		errors({ stack: true }),
+		colorize(),
+		timestamp({
+			format: 'YYYY-MM-DD HH:mm:ss',
+		}),
+		logFormat
+	),
 	transports: [new transports.Console()],
 });
 
-
-logger.log('info','test')
