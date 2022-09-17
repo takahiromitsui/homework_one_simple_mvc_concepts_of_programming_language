98fe355eb4ad0c88a26cdda6f4d1e0e1c3df0730
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-09-17 14:19:55 +0200

    Add error handling

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,27 +1,33 @@
-import { ExpressConf } from '../confObj';
+import { Request, Response } from 'express';
+import { customLogger, WinstonLevel } from '../logger';
 import { User } from '../models/user';
 
 const users: User[] = [];
 
-export const getDefault = (expressConf: ExpressConf) => {
-	expressConf.res.render('add-user', { pageTitle: 'Add User', path: '/' });
+export const getDefault = (req: Request, res: Response) => {
+	res.render('add-user', { pageTitle: 'Add User', path: '/' });
 };
 
-export const postUser = (expressConf: ExpressConf) => {
-	const username = expressConf.req.body.username;
+export const postUser = (req: Request, res: Response) => {
+	const username = req.body.username;
 	const id = new Date().toISOString();
 	const newUser: User = {
 		id: id,
 		name: username,
 	};
 	users.push(newUser);
-	expressConf.res.redirect(`/user/${id}`);
+	res.redirect(`/user/${id}`);
 };
 
-export const getUser = (expressConf: ExpressConf) => {
-	const id = expressConf.req.params.id;
+export const getUser = (req: Request, res: Response) => {
+	const id = req.params.id;
 	const user = users.find(user => user.id === id);
-	expressConf.res.render('user', {
+	if (!user) {
+		customLogger(WinstonLevel.ERROR, 'User not exist');
+		return res.redirect('/');
+	}
+	customLogger(WinstonLevel.INFO, 'User created');
+	res.render('user', {
 		pageTitle: 'User',
 		path: '/user',
 		username: user?.name || '',
